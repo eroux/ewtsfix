1534553542618bd8a3bed8204c768f1d29be1601
Elie Roux <elie.roux@telecom-bretagne.eu>
2017-09-01 11:15:41 +0200

    setup

---
+++ src/main/java/io/bdrc/Main.java
@@ -0,0 +1,66 @@
+package io.bdrc;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+import com.opencsv.CSVReader;
+import com.opencsv.CSVWriter;
+
+import io.bdrc.ewtsconverter.EwtsConverter;
+
+public class Main {
+	
+	public static int badLines = 0;
+	public static int allLines = 0;
+	public static EwtsConverter converter = new EwtsConverter();
+	
+	public static String[] handleLine(final String[] line) {
+		allLines += 1;
+		String firstPart = line[0];
+		if (firstPart.startsWith("OK:") || firstPart.startsWith("LNG:"))
+			return line;
+		final List<String> warns = new ArrayList<>();
+		converter.toUnicode(firstPart, warns, true);
+		if (warns.isEmpty())
+			return new String[] {"OK:"+firstPart};
+		badLines += 1;
+		StringBuilder warnsStr = new StringBuilder();
+		for (String warning : warns) {
+            warnsStr.append(warning.replace('"', '`').replace("line1: ", ""));
+        }
+		return new String[] {firstPart, warnsStr.toString()};
+	}
+	
+	public static void main( String[] args ) throws IOException {
+		
+		File corrFile = new File("corrections.csv");
+		FileReader corrFileReader = new FileReader(corrFile);
+		final CSVReader csvReader = new CSVReader(corrFileReader);
+		
+		List<String[]> newLines = new ArrayList<>();
+
+	     String [] nextLine;
+	     while ((nextLine = csvReader.readNext()) != null) {
+	        newLines.add(handleLine(nextLine));
+	     }
+	     
+	     csvReader.close();
+		corrFileReader.close();
+		
+		FileWriter fw = new FileWriter("corr2.csv");
+		CSVWriter csvWriter = new CSVWriter(fw);
+		
+		csvWriter.writeAll(newLines, true);
+		
+		csvWriter.close();
+		fw.close();
+		
+		System.out.println(badLines+" problematic on "+allLines+" (rate: "+(badLines/allLines)+")");
+		
+	}
+}
